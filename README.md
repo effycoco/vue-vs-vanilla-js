2812de19bad7793d0d34050fee0931fe4883a2bf
effycoco <734157786@qq.com>
2022-04-26 18:01:25 +0800

    rebuilt with vue

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-A simple project from [Udemy Course: Vue - The Complete Guide, Section 1](https://www.udemy.com/course/vuejs-2-the-complete-guide/)
+A simple project from [Vue - The Complete Guide, Section 1](https://www.udemy.com/course/vuejs-2-the-complete-guide/)
 
 First built with vanilla JS, then re-built with Vue.
+
+## What I Learned
+
+1.  ```js
+    {
+        data(){
+            return ;
+        }
+    }
+    ```
+    is the same as
+    ```js
+    {
+        data:(){
+            return ;
+        }
+    }
+    ```
+2.  `data`所返回的数据在 js 中使用时前面要加`this.`, 在 html 中使用时不需要加。js 中 this 指 data 返回的对象。
+
+## Reference
+
+1. [Use vue without a build step](https://vuejs.org/guide/quick-start.html#without-build-tools),
--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const addGoalBtn = document.querySelector("#app button");
-const ul = document.querySelector("ul");
-const goalInput = document.querySelector("#goal");
-addGoalBtn.addEventListener("click", addGoal);
-function addGoal() {
-  let li = document.createElement("li");
-  li.appendChild(document.createTextNode(goalInput.value));
-  // li.textContent = goalInput.value;
-  ul.appendChild(li);
-  goalInput.value = "";
-}
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      newGoal: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.newGoal);
+      this.newGoal = "";
+    },
+  },
+}).mount("#app");
+// const addGoalBtn = document.querySelector("#app button");
+// const ul = document.querySelector("ul");
+// const goalInput = document.querySelector("#goal");
+// addGoalBtn.addEventListener("click", addGoal);
+// function addGoal() {
+//   let li = document.createElement("li");
+//   li.appendChild(document.createTextNode(goalInput.value));
+//   // li.textContent = goalInput.value;
+//   ul.appendChild(li);
+//   goalInput.value = "";
+// }
